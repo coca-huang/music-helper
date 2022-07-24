12d5b5851fbb3bff37334a20dd5c6f7165eb94e8
Zhanbo Huang <40165523+MisakiCoca@users.noreply.github.com>
2022-07-24 16:50:54 +0800

    init basic workflow

---
+++ get-music.applescript
@@ -0,0 +1,42 @@
+on run {args}
+	tell application "Music"
+		try
+			-- get music state
+			if player state is not stopped then
+				set MusicState to "playing"
+			else
+				set MusicState to "paused"
+			end if
+			-- get music name and artist
+			set MusicName to name of current track
+			set MusicArtist to artist of current track
+			-- get music artwork
+			tell artwork 1 of current track
+				if format is JPEG picture then
+					set MusicArtworkType to ".jpg"
+				else
+					set MusicArtworkType to ".png"
+				end if
+			end tell
+			set MusicArtworkRaw to (get raw data of artwork 1 of current track)
+			-- save artwork image
+			set ArtworkPath to (args & "artwork" & MusicArtworkType) as text
+			try
+				tell me to set FileRef to (open for access ArtworkPath with write permission)
+				write MusicArtworkRaw to FileRef starting at 0
+				tell me to close access FileRef
+			on error m number n
+				display dialog "Image save failed." & m & n & ArtworkPath
+				try
+					tell me to close access FileRef
+				end try
+				return
+			end try
+			-- return as expected
+			return {MusicState, MusicName, MusicArtist, MusicArtworkType}
+		on error
+			display dialog "Music is not available."
+			return
+		end try
+	end tell
+end run
---
+++ main.py
@@ -0,0 +1,46 @@
+import logging
+import subprocess
+import time
+from argparse import ArgumentParser
+from pathlib import Path
+
+import schedule
+
+
+def refresh_meta(dst: str | Path):
+    logging.info(f'refresh music meta')
+    # get music meta with applescript
+    args = ["osascript", "-", f'{Path(dst).absolute()}/']
+    kwargs = dict(stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    proc = subprocess.Popen(args, **kwargs)
+    cmd = Path('get-music.applescript').read_text()
+    out, err = proc.communicate(cmd.encode('utf-8'))
+    out = out.decode('utf-8').rstrip()
+    try:
+        state, name, artist, a_type = [x.lstrip() for x in out.split(',')]
+        logging.debug(f'status: {state} | {name} - {artist}')
+    except Exception as err:
+        logging.error(f'error: {err}')
+        return
+    # save meta in dst folder
+    dst = Path(dst)
+    dst.mkdir(parents=True, exist_ok=True)
+    _ = [(dst / f'{k}.txt').write_text(v) for k, v in zip(['state', 'name', 'artist'], [state, name, artist])]
+
+
+if __name__ == "__main__":
+    # logging
+    logging.basicConfig(level=logging.DEBUG)
+
+    # parser
+    parser = ArgumentParser()
+    parser.add_argument('--dst', type=str, default='tmp', help='meta output folder')
+    args = parser.parse_args()
+
+    # jobs
+    schedule.every(2).seconds.do(refresh_meta, dst=args.dst)
+
+    # scheduler
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
